4dd0e057ae3201923d462fca9a8acbd1b88a7a4a
UsmonovAkobir <akobir0308@gmail.com>
2024-11-04 01:11:38 +0500

    Find the word!!!
    This game is written in python programming language. You will be able to find the word guessed by the computer by letters while playing this game. There are two versions of the game, one based on the Krill alphabet and the other based on the Latin alphabet.

---
+++ guessword/lesson9_3_soz_topish_oyini_2_qism_krill.py
@@ -0,0 +1,58 @@
+import random
+from uzwords import words
+
+
+def get_word():
+    
+    word = random.choice(words)
+    
+    while "-" in word or " " in word:
+        word = random.choice(words)
+        
+    return word.upper()
+
+
+def display_word(user_letters, word):
+    
+    display_letter = ""
+    
+    for letter in word:
+        if letter in user_letters:
+            display_letter += letter
+        else:
+            display_letter += "-"
+            
+    return display_letter
+
+
+def play():
+    
+    word = get_word()
+    
+    word_letters = set(word)
+    
+    user_letters = ""
+    print(f"Men {len(word)} ta harfdan iborat so'z o'yladim. Topa olasizmi?")
+    
+    while len(word_letters) > 0:
+        print(display_word(user_letters, word))
+        
+        if len(user_letters)> 0:
+            print(f"Shu paytgacha kiritgan harflaringiz: {user_letters}")
+        
+        letter = input("Harf kiriting: ").upper()
+        
+        if letter in user_letters:
+            print("Bu harfni oldin kiritgansiz. Boshqa harf kiriting.")
+            continue
+        
+        elif letter in word:
+            word_letters.remove(letter)
+            print(f"{letter} harfi to'g'ri.")
+            
+        else:
+            print("Bunday harf mavjud emas!")
+            
+        user_letters += letter
+    
+    print(f"Tabriklayman! {word} so'zini {len(user_letters)} urinishda topdingiz!!!")
